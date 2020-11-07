4410328bc1dc2e28eb8a8dba4802540a146bd21a
VirajS00 <virajshukla00@gmail.com>
2020-11-07 19:07:00 +0530

    small changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/urlinfo', async (req, res) => {
-	const getWikiData = new getWiki('https://en.wikipedia.org/wiki/Number');
+	const getWikiData = new getWiki('https://en.wikipedia.org/wiki/Pi');
 	const data = await getWikiData.getData();
 	res.send(data);
 });
