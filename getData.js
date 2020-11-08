6018552583d46dd1f2c6ef3982aab7f74c60be83
VirajS00 <virajshukla00@gmail.com>
2020-11-08 15:38:52 +0530

    added amazon

--- getData.js
+++ getData.js
@@ -15,20 +15,29 @@ module.exports = class getWiki {
 		}
 	}
 	async getData() {
+		const regexWiki = /^https?\:\/\/([\w\.]+)wikipedia.org\/wiki\/([\w]+\_?)+/;
+		const amazonRegex = /^https:\/\/www\.amazon\.(com|in)\/.+/gim;
 		try {
 			const html = await this.getHtml();
 			const $ = cheerio.load(html);
-			const title = $('#firstHeading').text();
-			const shortDesc = $('.shortdescription').text();
-			const icon = 'icons/wikipedia.ico';
-			let img = $('.thumbimage')[0];
-			let image;
-			if (img === undefined) {
-				image = undefined;
-			} else {
-				image = 'https:' + img.attribs.src;
+			if (regexWiki.test(this.url)) {
+				const title = $('#firstHeading').text();
+				const shortDesc = $('.shortdescription').text();
+				const icon = 'icons/wikipedia.ico';
+				let img = $('.thumbimage')[0];
+				let image;
+				if (img === undefined) {
+					image = undefined;
+				} else {
+					image = 'https:' + img.attribs.src;
+				}
+				return { title, shortDesc, image, icon, url: this.url };
+			} else if (amazonRegex.test(this.url)) {
+				const title = $('#productTitle').text().trim();
+				const image = $('.a-dynamic-image:first-child').attr('src');
+				const icon = 'icons/amazon_logo_1_0.png';
+				return { title, image, icon, url: this.url };
 			}
-			return { title, shortDesc, image, icon, url: this.url };
 		} catch {
 			return { error: 'error returning values' };
 		}
